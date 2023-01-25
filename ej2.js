e3c825d6e872a4ca198f789499e0daf0504b2231
facundo Mendez <facundo.mendez@live.com.ar>
2023-01-25 02:35:05 -0300

    terminar

--- ej2.js
+++ ej2.js
@@ -8,7 +8,21 @@ Dada una lista de elementos (array), crear una funcion que retorne una nueva lis
 */
 
 const pares = (array) => {
-    
+ 
+    let newArray = []
+    let count
+
+    for (let i = 0 ; i < array.length ; i++){
+        count = 0
+        for (let j = 0 ; j < array.length ; j++){
+
+         
+        }
+
+    }
+
+
+    return newArray
  
 }
 
